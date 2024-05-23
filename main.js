afa9052bcc5d32b883e3ee6aca8082d12799674c
Muhammad Naeem <164549465+naeem06@users.noreply.github.com>
2024-05-23 09:25:23 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,36 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+const answer = await inquirer.prompt([
+    { message: "Enter Your firstNumber", type: "number", name: "firstNumber" },
+    { message: "enter Your secondNumber", type: "number", name: "secondNumber" },
+    {
+        message: "Select One operators In List",
+        type: "list",
+        name: "operators",
+        choices: [
+            "Addition",
+            "Substraction",
+            "Multiplication",
+            "Division",
+            "Reminder",
+        ],
+    },
+]);
+if (answer.operators === "Addition") {
+    console.log(answer.firstNumber + answer.secondNumber);
+}
+else if (answer.operators === "Substraction") {
+    console.log(answer.firstNumber - answer.secondNumber);
+}
+else if (answer.operators === "Multiplication") {
+    console.log(answer.firstNumber * answer.secondNumber);
+}
+else if (answer.operators === "Division") {
+    console.log(answer.firstNumber / answer.secondNumber);
+}
+else if (answer.operators === "Reminder") {
+    console.log(answer.firstNumber % answer.secondNumber);
+}
+else {
+    console.log("incorrect");
+}
